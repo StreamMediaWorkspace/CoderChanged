efdfff4517cfe1a54d0f9963c9f45b0e13cc325c
yanghuatao <yanghuatao@100tal.com>
2020-02-19 12:11:38 +0800

    dd

--- Resources/windows/views/coder_webview.py
+++ Resources/windows/views/coder_webview.py
@@ -16,7 +16,7 @@ from classes import settings
 from classes.app import get_app
 from classes.logger import log
 
-class CoderWebView(QWebView, updates.UpdateInterface):
+class CoderWebView(QWebView):
 
     # Path to html file
     html_path = os.path.join(info.PATH, 'coderview', 'index.html')
@@ -38,10 +38,40 @@ class CoderWebView(QWebView, updates.UpdateInterface):
             return self.page().mainFrame().evaluateJavaScript(code)
 
     def setup_js_data(self):
+        print("=========coder webview====setup_js_data====")
         # Export self as a javascript object in webview
-        self.page().mainFrame().addToJavaScriptWindowObject('timeline', self)
+        self.page().mainFrame().addToJavaScriptWindowObject('coderview', self)
         self.page().mainFrame().addToJavaScriptWindowObject('mainWindow', self.window)
 
+    @pyqtSlot(str)
+    def qt_log(self, message=None):
+        log.info(message)
+
+    # This method is invoked by the UpdateManager each time a change happens (i.e UpdateInterface)
+    def changed(self, action):
+        return
+        # Remove unused action attribute (old_values)
+        action = deepcopy(action)
+        action.old_values = {}
+
+        # Send a JSON version of the UpdateAction to the timeline webview method: ApplyJsonDiff()
+        if action.type == "load":
+            # Initialize translated track name
+            _ = get_app()._tr
+            self.eval_js(JS_SCOPE_SELECTOR + ".SetTrackLabel('" + _("Track %s") + "');")
+
+            # Load entire project data
+            code = JS_SCOPE_SELECTOR + ".LoadJson(" + action.json() + ");"
+        else:
+            # Apply diff to part of project data
+            code = JS_SCOPE_SELECTOR + ".ApplyJsonDiff([" + action.json() + "]);"
+        self.eval_js(code)
+
+        # Reset the scale when loading new JSON
+        if action.type == "load":
+            # Set the scale again (to project setting)
+            initial_scale = get_app().project.get(["scale"]) or 15
+            get_app().window.sliderZoom.setValue(secondsToZoom(initial_scale))
 
     def __init__(self, window):
         QWebView.__init__(self)
@@ -57,7 +87,7 @@ class CoderWebView(QWebView, updates.UpdateInterface):
 
         # set url from configuration (QUrl takes absolute paths for file system paths, create from QFileInfo)
         self.setUrl(QUrl.fromLocalFile(QFileInfo(self.html_path).absoluteFilePath()))
-        
+
         # Connect signal of javascript initialization to our javascript reference init function
         self.page().mainFrame().javaScriptWindowObjectCleared.connect(self.setup_js_data)
 
